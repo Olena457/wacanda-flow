2d7c0503c5b265fabd35025b1f335a8483246b9f
Olena457 <olenademianenko42@gmail.com>
2025-09-09 18:38:07 +0300

    added photo poster

---
+++ src/data/posterData.js
@@ -0,0 +1,31 @@
+import namorHero from "../assets/images/poster/namor.jpg";
+import okoeHero from "../assets/images/poster/okoe.jpg";
+import ririHero from "../assets/images/poster/riri.jpg";
+import shuriHero from "../assets/images/poster/shuri.jpg";
+
+export const galleryPoster = [
+  {
+    id: 1,
+    name: "Shuri",
+    image: shuriHero,
+    youtubeUrl: "https://www.youtube.com/watch?v=aNz_DD-ZUc4",
+  },
+  {
+    id: 2,
+    name: "Namor",
+    image: namorHero,
+    youtubeUrl: "https://www.youtube.com/watch?v=2o4dJ3YhPoU",
+  },
+  {
+    id: 3,
+    name: "Riri",
+    image: ririHero,
+    youtubeUrl: "https://www.youtube.com/watch?v=2o4dJ3YhPoU",
+  },
+  {
+    id: 4,
+    name: "Okoe",
+    image: okoeHero,
+    youtubeUrl: "https://www.youtube.com/watch?v=2o4dJ3YhPoU",
+  },
+];
